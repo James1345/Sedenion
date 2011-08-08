2ca5723795a76576e8ea982f1a6346b64d2b8ac1
James McMahon <james1345@googlemail.com>
2011-08-08 22:37:28 +0100

    All compilation now handled by sbt. Complex class entirely scala and compiles.

--- src/main/scala/sedenion/Complex.scala
+++ src/main/scala/sedenion/Complex.scala
@@ -8,33 +8,36 @@ import java.lang.Math._;
  */
 class Complex( val re: Double, val im: Double ) {
   
-  def +(that: Any): Complex = that match {
-    case Complex => new Complex(re + that.re, im + that.im);
-    case Double => new Complex(re + that, im);
+  def +(that: Any):Complex = that match {
+    case that2: Complex => new Complex(re + that2.re, im + that2.im);
+    case that2: Double => new Complex(re + that2, im);
   }
   
-  def -(that: Any): Complex = this + (that * (-1));
+  def -(that: Any):Complex = that match {
+    case that2: Double => this + (that2 * (-1));
+	case that2: Complex => this + (that2 * (-1));
+  }
+  def unary_-: = new Complex(-re, -im);
   
-  def unary_-: Complex = new Complex(-re, -im);
-  
-  def *(that: Any): Complex = that match {
-    case Double => new Complex(re*that, im*that);
-    case Complex => new Complex(re*that.re + im*that.im, re*that.im + im*that.re);	
+  def *(that: Any):Complex = that match {
+    case that2: Double => new Complex(re*that2, im*that2);
+    case that2: Complex => new Complex(re*that2.re + im*that2.im, re*that2.im + im*that2.re);	
   }
   
   def /(that: Any):Complex = that match {
-    case Double => new Complex(re/that, im/that);
-	case Complex => new Complex((re*that.re + im*that.im)/(that.re*that.re + that.im*that.im), (im*that.re - re*that.im)/(that.re*that.re + that.im*that.im));
+    case that2: Double => new Complex(re/that2, im/that2);
+	case that2: Complex => new Complex((re*that2.re + im*that2.im)/(that2.re*that2.re + that2.im*that2.im), (im*that2.re - re*that2.im)/(that2.re*that2.re + that2.im*that2.im));
   }
   
   def abs = sqrt(re*re + im*im);
   
   def arg = atan2(im, re);
   
-  def *: Complex = new Complex(re, -im);
+  def * = new Complex(re, -im);
  
 }
 
+/* Try to get statics working 
 object Complex {
   def sqrt(c: Complex): Complex = {
     val gamma = java.lang.Math.sqrt((re+sqrt(re*re + im*im))/2);
@@ -42,3 +45,4 @@ object Complex {
   
   }
 }
+*/
