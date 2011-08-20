ee6e60868e2dc81bbb1232409de80bc6cb643660
James McMahon <james1345@googlemail.com>
2011-08-20 13:13:45 +0100

    Scala version of matrix class started in alpha folder. Will be branching to test different versions of matrix now.

--- src/main/scala/sedenion/alpha/Matrix.scala
+++ src/main/scala/sedenion/alpha/Matrix.scala
@@ -1,6 +1,6 @@
 package sedenion.alpha;
 
-class Matrix( val array: Array[Double], val cols: Int) {
+class Matrix( val array: Array[Double], val cols: Int){
 
 	// Constructor checking
 	if (0 != array.length % cols) {
@@ -11,13 +11,13 @@ class Matrix( val array: Array[Double], val cols: Int) {
 
 	def get(row: Int, col: Int): Double = array(row*cols + col);
 	
-	def +(that: Matrix): Matrix = {
-	    if ( rows != that.rows || cols != that.rows) {
+	def +(that: Matrix): Matrix ={
+	    if ( rows != that.rows || cols != that.cols) {
 			throw new IllegalArgumentException;
 		}
 		
 		val newArray = Array[Double](array.length);
-		for ( i <- 0 until array.length) {
+		for ( i <- 0 until (array.length - 1)){
 			newArray(i) = array(i) + that.array(i);
 		}
 		new Matrix(newArray, cols);
@@ -31,10 +31,39 @@ class Matrix( val array: Array[Double], val cols: Int) {
 		case that2: Int => new Matrix(array.map(_ * that2), cols);
 		case that2: Long => new Matrix(array.map(_ * that2), cols);
 		case that2: Short => new Matrix(array.map(_ * that2), cols);
-		case that2: Matrix => {
-			
+		case that2: Matrix =>{
+			val cols2 = that2.cols; // Quicker to type.
+			if(rows != cols2) throw new IllegalArgumentException("Matrix sizes incompatible.");
+			var newArray = new Array[Double](rows * cols2);
+			for ( i <- 0 until (rows-1) ) {
+				for ( j <- 0 until (cols2 - 1) ) {
+					newArray(i*cols2 + j) ={
+						var element = 0.0;
+						for ( k <- 0 until (cols-1) ) {
+							element += get(i, k) * that2.get(k, j);
+						}
+						element;
+					}
+				}
+			}
+			new Matrix(newArray, cols2);
 		}
-	}
+	} 
 	
-	class 
+	override def toString: String = {
+		
+		import java.lang.String.format;
+		
+		var thisString = "{ ";
+		
+		for ( i <- 0 until (cols - 1)) {
+			for ( j <- 0 until (rows - 1)) {
+				thisString += format("%4.2f,", get(i,j).asInstanceOf[AnyRef]); 
+			}
+			var length = thisString.length;
+			thisString = thisString.substring(0, length - 2); // Chop last character (trailing ,)
+			thisString += " }\n";
+		}
+		thisString;
+	}
 }
--- src/test/scala/Tester.scala
+++ src/test/scala/Tester.scala
@@ -1,12 +1,24 @@
-import sedenion._;
+import sedenion.alpha.Matrix;
 
 object Tester{
   def main(args: Array[String]) {
     
-	var c = new Complex(3.0, 4.0);
+	var arr = new Array[Double](4);
+	arr(0) = 1;
+	arr(1) = 2;
+	arr(2) = 3;
+	arr(3) = 4;
 	
-	println( c.toMatrix.toString() );
+	var arr2 = new Array[Double](4);
+	arr2(0) = 3;
+	arr2(1) = 1;
+	arr2(2) = 4;
+	arr2(3) = 1;
+	
+	var mat1 = new Matrix(arr, 2);
+	var mat2 = new Matrix(arr2, 2);
     
+	println((mat1).toString);
     }
 
 
