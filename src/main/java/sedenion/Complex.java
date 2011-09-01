3b0a9016b6d0b93d95e5f0edd4b4fd7701034f07
James McMahon <james1345@googlemail.com>
2011-09-01 20:58:27 +0100

    Complex now done in java

--- src/main/java/sedenion/Complex.java
+++ src/main/java/sedenion/Complex.java
@@ -52,4 +52,70 @@ public class Complex implements Extendable{
 	public Complex divide(Complex that){
 		new Complex((re*that.re + im*that.im)/(that.re*that.re + that.im*that.im), (im*that.re - re*that.im)/(that.re*that.re + that.im*that.im));
 	}
+
+	/** Raise this to the power of that */
+	public Complex pow(Complex that){
+		double r = Math.pow(abs(), that.re)*Math.pow(Math.E, -that.im*arg());
+		double theta = that.im*Math.log(abs())+that.re*arg();
+		return new Complex(r*Math.cos(theta), r*Math.sin(theta));		
+	}
+
+
+	public double arg(){
+		if(Double.NAN == arg) arg = Math.atan2(im, re);
+		return arg;
+	}
+
+	public String toString(){
+		if(null == toString) toString = "" + re + " " + im + "i";
+		return toString;
+	}
+
+	// Static Methods
+	
+	/** Returns the positive (primary) complex square root of a Complex Number (even if the number is entirely real or imaginary).
+   		The other square root can be found by -sqrt(myComplex)
+  	 */
+  	public static Complex sqrt(Complex c)  {
+		// Extract values and use common names
+		double x = c.re;
+		double y = c.im;
+		double r = c.abs;
+    		double gamma = sqrt((r+x)/2);
+		double delta = signumc(y)*sqrt((r-x)/2);
+		new Complex(gamma, delta);
+  	}
+
+	public static Complex sin(Complex c){
+		return new Complex(Math.sin(c.re)*Math.cosh(c.im), Math.cos(c.re)*Math.sinh(c.im));
+	} 
+
+	public static Complex cos(Complex c){
+		return new Complex(Math.cos(c.re)*Math.cosh(c.im), -Math.sin(c.re)*Math.sinh(c.im));
+	}
+
+	public static Complex tan(Complex c){
+		double a = 2*c.re;
+		double b = 2*c.im;
+		double alpha = Math.sin(a)/(Math.cos(a)+Math.cosh(b));
+		double beta = Math.sin(a)/((Math.cos(a)+Math.cosh(b));
+		return new Complex(alpha, beta);
+	}
+
+	public static Complex sinh(Complex c){
+		return new Complex(Math.sinh(c.re)*Math.cos(c.im), Math.sinh(c.re)*Math.sin(c.im));
+	} 
+
+	public static Complex cosh(Complex c){
+		return new Complex(Math.cosh(c.re)*Math.cos(c.im), Math.sinh(c.re)*Math.sin(c.im));
+	}
+
+	public static Complex tanh(Complex c) {
+  
+		double a = 2*c.re;
+		double b = 2*c.im;
+		double alpha = Math.sinh(a)/(Math.cosh(a)+Math.cos(b));
+		double beta = Math.sin(b)/(Math.cosh(a)+Math.cos(b));
+		new Complex(alpha, beta);
+	}
 }
